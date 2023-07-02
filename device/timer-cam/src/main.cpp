e6f0b962f803bb9b847cae3229a60d5508cfbb26
Yushin Ito <75526539+yushin-ito@users.noreply.github.com>
2023-07-02 23:21:45 +0900

    Add: TimerCamでSupabaseに画像を保存する機能を実装

--- device/timer-cam/src/main.cpp
+++ device/timer-cam/src/main.cpp
@@ -1,9 +1,103 @@
-#include <Arduino.h>
+#include "esp_camera.h"
+#include "camera_pins.h"
+
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <WiFiClientSecure.h>
+#include <base64.h>
+
+#include "env.h"
+
+const char* bucket = "image";
+
+bool init_camera(void) {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_SXGA;
+  config.jpeg_quality = 8;
+  config.fb_count = 2;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return false;
+  }
+
+  sensor_t* s = esp_camera_sensor_get();
+
+  s->set_vflip(s, 1);
+  s->set_brightness(s, 0);
+  s->set_saturation(s, 0);
+
+  return true;
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  WiFi.begin(SSID, PASSWORD);
+  Serial.print("WiFi connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  init_camera();
+
+  camera_fb_t* fb = esp_camera_fb_get();
+
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+  }
+
+  Serial.println("Capture completed.");
+
+  base64 base64;
+  String payload = base64.encode(fb->buf, fb->len);
+
+  WiFiClientSecure client;
+  client.setInsecure();
+
+  HTTPClient http;
+  http.begin(client, String(SUPABASE_URL) + "/storage/v1/object/image/avatar/image.jpg");
+  http.addHeader("Content-Type", "image/jpeg");
+  http.addHeader("apikey", String(SUPABASE_KEY));
+  http.addHeader("Authorization", "Bearer " + String(SUPABASE_KEY));
+  http.addHeader("Connection", "close");
+
+  int http_code = http.sendRequest("POST", fb->buf, fb->len);
+
+  http.end();
+  client.stop();
+
+  esp_camera_fb_return(fb);
+  delay(5000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(5 * 1000);
 }
